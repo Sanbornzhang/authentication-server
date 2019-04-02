498c17d8570f0f8c7d2e86e553091495919780f8
sanborn <z568817749@gmail.com>
2019-04-02 15:44:18 +0800

    change using middleware bind logger func -> using boot bind

--- middleware/logs.js
+++ middleware/logs.js
@@ -1,4 +1,3 @@
-const log4j = require('log4js')
 const process = require('process')
 /**
  * log middleware
@@ -7,28 +6,20 @@ const process = require('process')
  */
 async function logs(ctx, next) {
   // TODO: 验证参数的合法性 ctx.configOptions.log.options
-  log4j.configure(ctx.configOptions.log.options)
-  const logger = log4j.getLogger('default')
-  ctx.log = logger
+  const logger = ctx.logger
   const starTimer = process.hrtime()
   await next()
   const duration = process.hrtime(starTimer)
   const ms = duration[0] * 1000 + duration[1] / 1e6
   if (ctx.response && ctx.response.status < 400) {
-    logger.info(`method: ${ctx.request.method}, 
-                router: ${ctx.request.url}, 
-                status: ${ctx.response.status}, 
-                response time: ${ms} ms`)
-  }
-  else {
+    logger.info(
+      // eslint-disable-next-line max-len
+      `method: ${ctx.request.method},router: ${ctx.request.url},status: ${ctx.response.status},response time: ${ms} ms`)
+  } else {
     logger.error(
-      `method: ${ctx.request.method}, 
-                router: ${ctx.request.url}, 
-                status: ${ctx.response.status}, 
-                message: ${ctx.response.message}
-                response time: ${ms} ms`
+      // eslint-disable-next-line max-len
+      `method: ${ctx.request.method},router: ${ctx.request.url},status: ${ctx.response.status},message: ${ctx.response.message}`
     )
   }
-
 }
 module.exports = logs
