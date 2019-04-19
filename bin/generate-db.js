3b931a88dfaca7cfc90591fa44d5646c3e4406cd
Sanborn <z568817749@gmail.com>
2019-04-19 07:12:57 +0100

    change generate table function but it's will change to sql

--- bin/generate-db.js
+++ bin/generate-db.js
@@ -3,14 +3,24 @@ const instance = require('../boot/01-load-instance')
 const laodYaml = require('../utils/load-yaml')
 const config = laodYaml(path.join(__dirname, '../config.yaml'))
 const fakeApp = {context: {configOptions: config}}
-instance(fakeApp)
-.then((Models)=>{
-  const creatDBList = []
-  Object.keys(Models).forEach(modeName=>{
-    creatDBList.push(Models[modeName].model.sync())
+const creatTables = []
+const creatRelationsTables = []
+/**
+ * main create table
+ */
+async function main() {
+  Models = await instance(fakeApp)
+  // TODO: 关联关系表创建不会成功！ mysql 集群中外键的性能会比较慢.
+  Object.keys(Models).forEach(async modeName=>{
+    if (modeName.toString().split('_').length === 1) {
+      await Models[modeName].model.sync()
+    } else {
+      creatRelationsTables.push(Models[modeName].model)
+    }
   })
-  return Promise.all(creatDBList)
-})
-.then(_=>{
+  for (const model of creatRelationsTables) {
+    await model.sync()
+  }
   console.log('create tables successful')
-})
+}
+main()
